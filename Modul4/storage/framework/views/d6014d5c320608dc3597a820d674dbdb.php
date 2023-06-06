8c99949cea6e3d2cbfb362f3213cc010d40ccec8
Naoepal <86772933+Naoepal@users.noreply.github.com>
2023-06-07 00:07:19 +0700

    Add files via upload

---
+++ Modul4/storage/framework/views/d6014d5c320608dc3597a820d674dbdb.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('title', __('Not Found')); ?>
+<?php $__env->startSection('code', '404'); ?>
+<?php $__env->startSection('message', __('Not Found')); ?>
+
+<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\Mamad\P_framework\Praktikum PF_Modul4_1204200061_Naufal Rakhmad\Modul4\vendor\laravel\framework\src\Illuminate\Foundation\Exceptions/views/404.blade.php ENDPATH**/ ?>
